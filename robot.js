4244d89a49ce839050b2a587dcb562d9a14c49bc
Federico <federicoc03@gmail.com>
2023-05-24 17:34:39 -0500

    Challenge Done

--- robot.js
+++ robot.js
@@ -1,51 +1,60 @@
 function createRobot(coordinates, direction) {
-  let [x, y] = coordinates
-  let orientation = direction 
+  let [x, y] = coordinates;
+  let orientation = direction;
 
   return {
     getPosition: () => {
-      return { coordinates: [x, y], orientation }
+      return { coordinates: [x, y], orientation };
     },
     advance: () => {
-      switch (orientation) {
-        case "North": 
-          coordinates[1] = coordinates[1] + 1;
-          break; 
-        case "South": 
-          coordinates[1] = coordinates[1] -1; 
-          break; 
-        case "East":
-          coordinates[0] = coordinates[0] + 1; 
-          break; 
-        case "West": 
-          coordinates[0] = coordinates[0] - 1;
-          break; 
+      try {
+        switch (orientation) {
+          case "North":
+            coordinates[1] = coordinates[1] + 1;
+            break;
+          case "South":
+            coordinates[1] = coordinates[1] - 1;
+            break;
+          case "East":
+            coordinates[0] = coordinates[0] + 1;
+            break;
+          case "West":
+            coordinates[0] = coordinates[0] - 1;
+            break;
+        }
+        if (
+          coordinates[0] < 0 ||
+          coordinates[0] > 10 ||
+          coordinates[1] < 0 ||
+          coordinates[1] > 10
+        ) {
+          throw new Error("No more space to move");
+        }
+        return { coordinates: coordinates, orientation };
+      } catch (error) {
+        return error.message;
       }
-      if (coordinates[0]<0 || coordinates[0]>10 || coordinates[1]<0 || coordinates[1]>10) {
-        throw new Error("No more space to move");
-      }
-      return { coordinates: coordinates, orientation }
     },
-    turnRight:  () => {
+    turnRight: () => {
       switch (orientation) {
-        case "North": 
+        case "North":
           orientation = "East";
-          break; 
-        case "East": 
+          break;
+        case "East":
           orientation = "South";
-          break; 
+          break;
         case "South":
           orientation = "West";
-          break; 
-        case "West": 
+          break;
+        case "West":
           orientation = "North";
-          break; 
+          break;
       }
-      return { coordinates: [x,y], orientation: orientation }
+      return { coordinates: [x, y], orientation: orientation };
     },
     turnLeft: () => {
       switch (orientation) {
-        case "North": 
+        case "North":
           orientation = "West";
           break;
         case "West":
@@ -58,40 +67,42 @@ function createRobot(coordinates, direction) {
           orientation = "North";
           break;
       }
-      return { coordinates: [x,y], orientation: orientation }
+      return { coordinates: [x, y], orientation: orientation };
     },
     instructions: function (stringInstructions) {
-      instructions = stringInstructions.split('')
-      instructions.forEach(instruction => {
-        switch (instruction) {
-          case "R":
-            console.log("TurnRight");
-            this.turnRight();
-            console.log({ coordinates: coordinates, orientation: orientation })
-            break;
-          case "A":
-            console.log("Avanza");
-            this.advance();
-            console.log({ coordinates: coordinates, orientation: orientation })
-            break;
-          case "L":
-            this.turnLeft();
-            console.log("TurnLeft");
-            console.log({ coordinates: coordinates, orientation: orientation })
-            break;
-          default:
-            console.log("Unknown instruction");
-        }
-      })
-      
-    return { coordinates: coordinates, orientation: orientation }        
-    }
-  }
+      try {
+        instructions = stringInstructions.split("");
+        instructions.forEach((instruction) => {
+          switch (instruction) {
+            case "R":
+              // console.log("TurnRight");
+              this.turnRight();
+              // console.log({ coordinates: coordinates, orientation: orientation })
+              break;
+            case "A":
+              // console.log("Avanza");
+              this.advance();
+              // console.log({ coordinates: coordinates, orientation: orientation })
+              break;
+            case "L":
+              // console.log("TurnLeft");
+              this.turnLeft();
+              // console.log({ coordinates: coordinates, orientation: orientation })
+              break;
+            default:
+              throw new Error("Only known instructions are supported");
+          }
+        });
+        return { coordinates: coordinates, orientation: orientation };
+      } catch (error) {
+        return error.message;
+      }
+    },
+  };
 }
 
-module.exports = {createRobot}
-
+module.exports = { createRobot };
 
 const robot = createRobot([7, 3], "North");
 
-console.log(robot.instructions("RAALAL"))
+console.log(robot.turnLeft());
--- robot.test.js
+++ robot.test.js
@@ -1,24 +1,52 @@
 const { createRobot } = require('./robot.js');
 
-describe('Robot', () => {
-	test('The robot advances 1 North', () => {
-		// Arrange
-		const expected = { coordinates: [ 0, 1 ], orientation: 'North' }
-		//Act
-		const result = createRobot([0,0], "North").advance();
-		//Assert
-		expect(result).toEqual(expected)
-	});
-
+describe('getPosition function tests', () => {
 	test('We get the position of the robot', () => {
 		const expected = { coordinates: [ 1,3 ], orientation: 'North' }
 		const result = createRobot([1,3], "North").getPosition();
 		expect(result).toEqual(expected)
 	})
+});
 
-	test('We move the robot with the innstructions', () => {
-		const expected = { coordinates: [9, 4], orientation: "West"}
-		const result = createRobot([7, 3],"North").getPosition();
+describe('advance function tests', () => {
+	test('The robot advances 1 North', () => {
+		// Arrange
+		const expected = { coordinates: [ 0, 1 ], orientation: 'North' }
+		//Act
+		const result = createRobot([0,0], 'North').advance();
+		//Assert
+		expect(result).toEqual(expected)
+	});
+
+	test('An error should appear when surpassing the limit', () => {
+		const expected = "No more space to move"
+		const result = createRobot([0,10], 'North').advance();
+		expect(result).toMatch(expected)
+	})
+})
+
+describe('instructions function tests', () => {
+	test('Should be able to advance and rotate right in all directions', () => {
+		const expected = {coordinates: [0,0], orientation: 'North'}
+		const result = createRobot([0,0], 'North').instructions('ARARARAR');
 		expect(result).toEqual(expected)
 	})
-});
+
+	test('Should be able to rotate left in all directions', () => {
+		const expected = {coordinates: [0,0], orientation: 'North'}
+		const result = createRobot([0,0], 'North').instructions('LLLL');
+		expect(result).toEqual(expected)
+	})
+
+	test('We move the robot with the innstructions in the challenge', () => {
+		const expected = { coordinates: [9, 4], orientation: "West"}
+		const result = createRobot([7, 3],"North").instructions("RAALAL");
+		expect(result).toEqual(expected)
+	})
+
+	test('Should throw an error when something different to R, L or A is encountered', () => {
+		const expected = "Only known instructions are supported"
+		const result = createRobot([0,0], "North").instructions("ASDFG");
+		expect(result).toMatch(expected)
+	})
+})
