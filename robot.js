fd061ebad7a3c54f40f5b7722462673ec8ca6ef2
Federico <federicoc03@gmail.com>
2023-05-24 12:57:20 -0500

    Exercise halfway done

--- robot.js
+++ robot.js
@@ -7,22 +7,91 @@ function createRobot(coordinates, direction) {
       return { coordinates: [x, y], orientation }
     },
     advance: () => {
-      // función para avanzar
+      switch (orientation) {
+        case "North": 
+          coordinates[1] = coordinates[1] + 1;
+          break; 
+        case "South": 
+          coordinates[1] = coordinates[1] -1; 
+          break; 
+        case "East":
+          coordinates[0] = coordinates[0] + 1; 
+          break; 
+        case "West": 
+          coordinates[0] = coordinates[0] - 1;
+          break; 
+      }
+      if (coordinates[0]<0 || coordinates[0]>10 || coordinates[1]<0 || coordinates[1]>10) {
+        throw new Error("No more space to move");
+      }
+      return { coordinates: coordinates, orientation }
     },
-    turnRight: () => {
-      // función para girar a la derecha
+    turnRight:  () => {
+      switch (orientation) {
+        case "North": 
+          orientation = "East";
+          break; 
+        case "East": 
+          orientation = "South";
+          break; 
+        case "South":
+          orientation = "West";
+          break; 
+        case "West": 
+          orientation = "North";
+          break; 
+      }
+      return { coordinates: [x,y], orientation: orientation }
     },
     turnLeft: () => {
-      // función para girar a la izquierda
+      switch (orientation) {
+        case "North": 
+          orientation = "West";
+          break;
+        case "West":
+          orientation = "South";
+          break;
+        case "South":
+          orientation = "East";
+          break;
+        case "East":
+          orientation = "North";
+          break;
+      }
+      return { coordinates: [x,y], orientation: orientation }
     },
-    instructions: (stringInstructions) => {
-      // función para recibir una serie de instrucciones Ejm: "RAALAL"
-      // En este metodo se va crear 
+    instructions: function (stringInstructions) {
+      instructions = stringInstructions.split('')
+      instructions.forEach(instruction => {
+        switch (instruction) {
+          case "R":
+            console.log("TurnRight");
+            this.turnRight();
+            console.log({ coordinates: coordinates, orientation: orientation })
+            break;
+          case "A":
+            console.log("Avanza");
+            this.advance();
+            console.log({ coordinates: coordinates, orientation: orientation })
+            break;
+          case "L":
+            this.turnLeft();
+            console.log("TurnLeft");
+            console.log({ coordinates: coordinates, orientation: orientation })
+            break;
+          default:
+            console.log("Unknown instruction");
+        }
+      })
+      
+    return { coordinates: coordinates, orientation: orientation }        
     }
   }
 }
 
-module.exports = createRobot
+module.exports = {createRobot}
 
 
-console.log(createRobot([2,3], "North").getPosition());
+const robot = createRobot([7, 3], "North");
+
+console.log(robot.instructions("RAALAL"))
--- robot.test.js
+++ robot.test.js
@@ -1,7 +1,24 @@
 const { createRobot } = require('./robot.js');
 
 describe('Robot', () => {
-	test('Cambia el titulo y empieza a escribir tu primera prueba', () => {
-
+	test('The robot advances 1 North', () => {
+		// Arrange
+		const expected = { coordinates: [ 0, 1 ], orientation: 'North' }
+		//Act
+		const result = createRobot([0,0], "North").advance();
+		//Assert
+		expect(result).toEqual(expected)
 	});
+
+	test('We get the position of the robot', () => {
+		const expected = { coordinates: [ 1,3 ], orientation: 'North' }
+		const result = createRobot([1,3], "North").getPosition();
+		expect(result).toEqual(expected)
+	})
+
+	test('We move the robot with the innstructions', () => {
+		const expected = { coordinates: [9, 4], orientation: "West"}
+		const result = createRobot([7, 3],"North").getPosition();
+		expect(result).toEqual(expected)
+	})
 });
